1c9f322636271aaf5f2e8ca4b473c4e2bda12766
Marcos Ribeiro <slproger@gmail.com>
2022-04-09 06:27:08 -0300

    Testando paginação via MySQL

--- bin/test/pessoa_repository_test.dart
+++ bin/test/pessoa_repository_test.dart
@@ -12,10 +12,14 @@ void main() {
   var datasource = MysqlDatasourceMock();
   var repository = PessoaRepository(datasource);
   var retorno = [
-    {
-      "ID": "1",
-      "NMPESSOA": "MAKEMALTE MAQUIAGENS E ESMALTES EIRELI 5",
-    }
+    {"ID": "1", "NMPESSOA": "MAKEMALTE MAQUIAGENS E ESMALTES EIRELI 1"},
+    {"ID": "2", "NMPESSOA": "MAKEMALTE MAQUIAGENS E ESMALTES EIRELI 2"},
+    {"ID": "3", "NMPESSOA": "MAKEMALTE MAQUIAGENS E ESMALTES EIRELI 3"},
+    {"ID": "4", "NMPESSOA": "MAKEMALTE MAQUIAGENS E ESMALTES EIRELI 4"},
+    {"ID": "5", "NMPESSOA": "MAKEMALTE MAQUIAGENS E ESMALTES EIRELI 5"},
+    {"ID": "6", "NMPESSOA": "MAKEMALTE MAQUIAGENS E ESMALTES EIRELI 6"},
+    {"ID": "7", "NMPESSOA": "MAKEMALTE MAQUIAGENS E ESMALTES EIRELI 7"},
+    {"ID": "8", "NMPESSOA": "MAKEMALTE MAQUIAGENS E ESMALTES EIRELI 8"},
   ];
   test("Deve retornar uma Lista de Pessoas", () async {
     var sql = "SELECT * FROM ${PessoaModel.nomeTabela}";
@@ -25,6 +29,20 @@ void main() {
     var lista = resp.data;
     expect(lista, isA<List<PessoaModel>>());
     expect(lista[0].nmpessoa, "MAKEMALTE MAQUIAGENS E ESMALTES EIRELI 5");
+  });
+
+  test('Deve retornar uma pagina com 5 registros', () async {
+    var datasource = MysqlDatasource();
+    var repository = PessoaRepository(datasource);
+    // var sql = "SELECT * FROM ${PessoaModel.nomeTabela}";
+    // when(() => datasource
+    //         .query(sql: sql,))
+    //     .thenAnswer((_) async => ResponseSQL(retorno));
+    var resp =
+        await repository.findAll(paginator: {"pagina": "2", "limit": "5"});
+    var lista = resp.data;
+    expect(lista, isA<List<PessoaModel>>());
+    expect(lista.length, 5);
   });
 
   test("Deve retornar um Model de Pessoa", () async {
