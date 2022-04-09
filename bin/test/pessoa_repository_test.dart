77b9744d0980c9c1fa1906ffd07cc52866598ad9
Marcos Ribeiro <slproger@gmail.com>
2022-04-09 06:35:22 -0300

    Ajustes nos testes

--- bin/test/pessoa_repository_test.dart
+++ bin/test/pessoa_repository_test.dart
@@ -28,7 +28,7 @@ void main() {
     var resp = await repository.findAll();
     var lista = resp.data;
     expect(lista, isA<List<PessoaModel>>());
-    expect(lista[0].nmpessoa, "MAKEMALTE MAQUIAGENS E ESMALTES EIRELI 5");
+    expect(lista[0].nmpessoa, retorno[0]["NMPESSOA"]);
   });
 
   test('Deve retornar uma pagina com 5 registros', () async {
@@ -53,6 +53,6 @@ void main() {
     var resp = await repository.findById("1");
     PessoaModel model = resp.data;
     expect(model, isA<PessoaModel>());
-    expect(model.nmpessoa, "MAKEMALTE MAQUIAGENS E ESMALTES EIRELI 5");
+    expect(model.nmpessoa, retorno[0]["NMPESSOA"]);
   });
 }
